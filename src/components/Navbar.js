a5d37dda6a66ec87a886d7677c9922d4724a51e0
Alexander <ahansen1335@gmail.com>
2022-07-13 08:02:23 +0200

    add logo to navbar with Link to home page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../images/Logo.svg';
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav>
+      <Link to='/'>
+        <div className='logo'>{<Logo />}</div>
+      </Link>
+    </nav>
   )
 }
