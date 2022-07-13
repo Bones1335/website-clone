5e0625b9af54ebe1a65ec591ad24f888c6f13909
Alexander <ahansen1335@gmail.com>
2022-07-13 08:08:12 +0200

    create chat icon link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../images/Logo.svg';
+import { ReactComponent as Chat } from '../images/chat.svg';
 
 export default function Navbar() {
   return (
@@ -8,6 +9,11 @@ export default function Navbar() {
       <Link to='/'>
         <div className='logo'>{<Logo />}</div>
       </Link>
+      <ul>
+        <Link to='/messages'>
+          <li>{<Chat />}</li>
+        </Link>
+      </ul>
     </nav>
   )
 }
