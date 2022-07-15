fc5fc139760de480fb49e05ee0e08ea83ac940c1
Alexander <ahansen1335@gmail.com>
2022-07-15 14:26:28 +0200

    create basic Create Post div with profile pic input and image post divisions

---
+++ src/components/home/CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CreatePost() {
+  return (
+    <div className="create-post-container">
+        <img src='#' alt='profile pic'></img>
+        <input type='text' placeholder='Create Post'></input>
+        <button type='button'>Create Image Post</button>
+    </div>
+  )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React from 'react';
+import CreatePost from '../components/home/CreatePost';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <CreatePost />
   )
 }
