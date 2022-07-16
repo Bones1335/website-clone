810bfd3b91b88a69e90102c46876e37b5750b592
Alexander <ahansen1335@gmail.com>
2022-07-16 09:56:17 +0200

    style create post div

--- src/components/home/CreatePost.js
+++ src/components/home/CreatePost.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import Image from '../../images/Image.png';
 
 export default function CreatePost() {
   return (
     <div className="create-post-container">
-        <img src='#' alt='profile pic'></img>   
+        <img className='profile-picture' src='#' alt='profile pic'></img>   
         <input className="create-post" type='text' placeholder='Create Post'></input>
-        <button type='button'>Create Image Post</button>
+        <button className="create-image-button" type='button'>
+          <img className="create-image-button" src={Image} alt='Create Img Post'></img>
+        </button>
     </div>
   )
 }
