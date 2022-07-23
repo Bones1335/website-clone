f557f3dec2dd27179d70dbfd6e77aedd36214a9f
Alexander <ahansen1335@gmail.com>
2022-07-23 08:25:32 +0200

    create input form for Itredd posts with cancel functionality and console log submit message

--- src/components/home/CreatePost.js
+++ src/components/home/CreatePost.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from '../../images/Image.png';
+import PostForm from './PostForm';
+
 
 export default function CreatePost() {
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const handleClick = () => {
+    togglePopup();    
+  }
+
   return (
-    <div className="create-post-container">
-        <img className='profile-picture' src='#' alt='profile pic'></img>   
-        <input className="create-post" type='text' placeholder='Create Post'></input>
-        <button className="create-image-button" type='button'>
-          <img className="create-image-button" src={Image} alt='Create Img Post'></img>
-        </button>
-    </div>
+    <>
+      <div className="create-post-container">
+          <img className='profile-picture' src='#' alt='profile pic'></img>   
+          <input className="create-post" placeholder='Create Post' onClick={handleClick}></input>
+          <button className="create-image-button" type='button'>
+            <img className="create-image-button" src={Image} alt='Create Img Post'></img>
+          </button>
+      </div>
+      {isOpen && <PostForm togglePopup={togglePopup} />}
+    </>
   )
 }
--- src/components/home/PostForm.js
+++ src/components/home/PostForm.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function PostForm() {
+export default function PostForm({togglePopup}) {
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+  }
+  
   return (
-    <div>
-        <form>
+    <div className='post-form-container'>
+        <form onSubmit={handleSubmit}>
             <h3>Create Post:</h3>
-            <label>Title: </label>
-                <input type='text'></input>
+            <label for="title">Title: </label>
+                <input name="title" id="title" type='text'></input>
+            <label for="post-input">Post: </label>
+                <textarea name="post-input" id="title"></textarea>
+            <div className="post-buttons">
+              <button id="submit-button" type="submit">Post</button>
+              <button id="cancel-button" type="button" onClick={togglePopup}>Cancel</button>
+            </div>
         </form>
     </div>
   )
